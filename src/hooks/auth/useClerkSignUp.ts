2815ca0a227e0016327f39ece550e700ac286444
ImDuy <158559509+ImDuy@users.noreply.github.com>
2024-09-13 16:26:03 +0700

    add useCallback to avoid potential infinite loop in components useEffect

--- src/hooks/auth/useClerkSignUp.ts
+++ src/hooks/auth/useClerkSignUp.ts
@@ -1,5 +1,5 @@
 import { useSignUp } from "@clerk/clerk-expo";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { AuthFormInfo } from "../../utils/types";
 import { setIsLoading } from "../../store/appSlice";
@@ -19,82 +19,90 @@ export default function useClerkSignUp() {
     error: "",
   });
 
-  const clerkSignUp = async (signUpForm: AuthFormInfo) => {
-    if (!isLoaded) return;
+  const clerkSignUp = useCallback(
+    async (signUpForm: AuthFormInfo) => {
+      if (!isLoaded) return;
 
-    dispatch(setIsLoading(true));
-    try {
-      await signUp.create({
-        emailAddress: signUpForm.email,
-        password: signUpForm.password,
-      });
-
-      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
-      dispatch(setIsLoading(false));
-      setUserInfo(signUpForm);
-      setVerification((prevState) => ({ ...prevState, state: "pending" }));
-    } catch (err: any) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      dispatch(setIsLoading(false));
-      Alert.alert("Error", err.errors[0].longMessage);
-    }
-  };
-
-  const clerkVerification = async (code: string) => {
-    if (!isLoaded) return;
-
-    dispatch(setIsLoading(true));
-    try {
-      const completeSignUp = await signUp.attemptEmailAddressVerification({
-        code,
-      });
-      if (completeSignUp.status === "complete") {
-        // create user in database
-        await fetchAPI("/user", {
-          method: "POST",
-          body: JSON.stringify({
-            name: userInfo?.name,
-            email: userInfo?.email,
-            clerkId: completeSignUp.id,
-          }),
+      dispatch(setIsLoading(true));
+      try {
+        await signUp.create({
+          emailAddress: signUpForm.email,
+          password: signUpForm.password,
         });
-        await setActive({ session: completeSignUp.createdSessionId });
+
+        await signUp.prepareEmailAddressVerification({
+          strategy: "email_code",
+        });
         dispatch(setIsLoading(false));
-        setVerification((prevState) => ({
-          ...prevState,
-          state: "success",
-          signInSessionId: completeSignUp.createdSessionId,
-        }));
-      } else {
-        setVerification((prevState) => ({
-          ...prevState,
-          error: "Verification failed",
-        }));
+        setUserInfo(signUpForm);
+        setVerification((prevState) => ({ ...prevState, state: "pending" }));
+      } catch (err: any) {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        dispatch(setIsLoading(false));
+        Alert.alert("Error", err.errors[0].longMessage);
       }
-    } catch (err: any) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      dispatch(setIsLoading(false));
+    },
+    [isLoaded, signUp, dispatch]
+  );
 
-      if (err instanceof TypeError) {
-        setVerification((prevState) => ({
-          ...prevState,
-          error: err.message,
-        }));
-      } else {
-        setVerification((prevState) => ({
-          ...prevState,
-          error: err?.errors[0]?.longMessage,
-        }));
+  const clerkVerification = useCallback(
+    async (code: string) => {
+      if (!isLoaded) return;
+
+      dispatch(setIsLoading(true));
+      try {
+        const completeSignUp = await signUp.attemptEmailAddressVerification({
+          code,
+        });
+        if (completeSignUp.status === "complete") {
+          // create user in database
+          await fetchAPI("/user", {
+            method: "POST",
+            body: JSON.stringify({
+              name: userInfo?.name,
+              email: userInfo?.email,
+              clerkId: completeSignUp.id,
+            }),
+          });
+          await setActive({ session: completeSignUp.createdSessionId });
+          dispatch(setIsLoading(false));
+          setVerification((prevState) => ({
+            ...prevState,
+            state: "success",
+            signInSessionId: completeSignUp.createdSessionId,
+          }));
+        } else {
+          setVerification((prevState) => ({
+            ...prevState,
+            error: "Verification failed",
+          }));
+        }
+      } catch (err: any) {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        dispatch(setIsLoading(false));
+
+        if (err instanceof TypeError) {
+          setVerification((prevState) => ({
+            ...prevState,
+            error: err.message,
+          }));
+        } else {
+          setVerification((prevState) => ({
+            ...prevState,
+            error: err?.errors[0]?.longMessage,
+          }));
+        }
       }
-    }
-  };
+    },
+    [isLoaded, signUp, dispatch, userInfo, setActive]
+  );
 
-  const clearSignUpData = () => {
+  const clearSignUpData = useCallback(() => {
     setUserInfo(undefined);
     setVerification({ state: "", error: "" });
-  };
+  }, []);
 
   return {
     clerkSignUp,
